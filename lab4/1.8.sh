94d12e83c700ebcb2f7f2f18032ee18bbea40d4b
MaxG175 <55529888+MaxG175@users.noreply.github.com>
2019-12-11 18:37:46 +0300

    Delete 1.8.sh

--- lab4/1.8.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-amount_of_process_user=$(ps -U akhudoyarova -u akhudoyarova u | wc -l)
-amount_of_process_root=$(ps -U root -u root u | wc -l)
-echo "Процессов пользователя:" 
-echo $amount_of_process_user
-echo "Процессов пользователя root:"
-echo $amount_of_process_root
